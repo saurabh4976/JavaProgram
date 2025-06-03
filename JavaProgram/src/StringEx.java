4cca968b1069689dfa12c1891dbc24ef254a4ec5
saurabh4976 <saurabh1308sp@gmail.com>
2025-06-03 09:40:04 +0530

    add new program

--- JavaProgram/src/StringEx.java
+++ JavaProgram/src/StringEx.java
@@ -1,11 +1,43 @@
 public class StringEx {
     public static void main(String[] args) {
-        String a = "dev";
-        String b = "dev";
-        String s = new String("dev");
+//        String a = "dev";
+//        String b = "dev";
+//        String s = new String("dev");
+//
+//        System.out.println(a == b);
+//        System.out.println(a == s.intern());
 
-        System.out.println(a == b);
-        System.out.println(a == s.intern());
+        String brand = "Dev";
+        String courseName = new String("Dev");
+
+        System.out.println(brand);
+        System.out.println(courseName);
+
+        brand = "Devand";
+        System.out.println(brand);
+        System.out.println(courseName);
+
+        String brand1 = "Honda";
+        String brand2 = "Honda";
+
+
+
+
+        String xyz = "hello";
+        String wxyz = "hello";
+
+        System.out.println(xyz == wxyz);
+        System.out.println(xyz.equals(wxyz));
+
+
+
+        String xyz2 = new String("FOO");
+        String wxyz2 = new String("FOO");
+
+        System.out.println(xyz2 == wxyz2);
+        System.out.println(xyz2.equals(wxyz2));
+
+
 
 
     }
